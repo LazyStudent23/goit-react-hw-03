c95047c094da89a434288ae2022400043bb019bb
LazyStudent23 <wizard.alex2016@gmail.com>
2024-10-09 02:19:26 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 import { useState, useEffect } from "react";
+
+import initialContacts from "./initial-contacts.json";
+
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    JSON.parse(window.localStorage.getItem("storage-contacts")) &&
+      initialContacts;
+  });
 
-  return <></>;
-}
+  useEffect(() => {
+    window.localStorage.setItem("storage-contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const [searchedValue, setSearchedValue] = useState("");
+
+  const searchedContacts = contacts.filter((user) =>
+    user.name.toLowerCase().includes(searchedValue.toLowerCase().trim())
+  );
+
+  const addContact = (newContact) => {
+    setContacts((contacts) => {
+      return [...contacts, newContact];
+    });
+  };
+
+  const deleteContact = (clickedContactId) => {
+    setContacts((contacts) => {
+      return contacts.filter((contact) => contact.id !== clickedContactId);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={addContact} />
+      {contacts.length !== 0 && (
+        <SearchBox value={searchedValue} onSearch={setSearchedValue} />
+      )}
+      <ContactList contacts={searchedContacts} onDelete={deleteContact} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import { BiSolidUser } from "react-icons/bi";
+import { MdOutlinePhoneIphone } from "react-icons/md";
+
+import styles from "./Contact.module.css";
+
+const Contact = ({ user, onDelete }) => {
+  const { name, number, id } = user;
+  return (
+    <>
+      <div className={styles.details}>
+        <p className={styles.info}>
+          <BiSolidUser className={styles.icon} />
+          {name}
+        </p>
+        <p className={styles.info}>
+          <MdOutlinePhoneIphone className={styles.icon} />
+          {number}
+        </p>
+        <button
+          onClick={() => {
+            onDelete(id);
+          }}
+          className={styles.btn}
+          type="button"
+        >
+          Delete
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
